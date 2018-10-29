d0dd20f49409d8ec57fd12f7fff961f5f859d54a
Kelvin Cortez <kelvcortez@gmail.com>
2018-10-29 15:47:29 +1300

    Add project files.

---
+++ TestBibleForum.Data/IForum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using
+using TestBibleForum.Data.Models;
+using System.Threading.Tasks;
+
+namespace TestBibleForum.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetApplicationUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+
+    }
+}
---
+++ TestBibleForum.Service/ForumService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestBibleForum.Data;
+using TestBibleForum.Data.Models;
+
+namespace TestBibleForum.Service
+{
+    public class ForumService : IForum
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ForumService(ApplicationDbContext DbContext)
+        {
+            _dbContext = DbContext;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _dbContext.Forums
+                .Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetApplicationUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
