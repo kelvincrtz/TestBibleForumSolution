05ba4c9cafc23650988c4a44cf5c18fbbe7654bf
Kelvin Cortez <kelvcortez@gmail.com>
2018-10-29 15:54:20 +1300

    ForumController

---
+++ TestBibleForum/Controllers/ForumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestBibleForum.Data;
+
+namespace TestBibleForum.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
